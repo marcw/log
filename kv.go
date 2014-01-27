95223f31c4e5dc056eb9802231f788836e5a8571
Marc Weistroff <marc@weistroff.net>
2014-01-27 17:19:27 +0100

    Add key=value marshalization

---
+++ kv.go
@@ -0,0 +1,147 @@
+package gogol
+
+import (
+	"bytes"
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type encodeState struct {
+	buffer     *bytes.Buffer
+	writeBegan bool
+}
+
+// Marshal will encode any structure or map in a k=v format
+func Marshal(data interface{}) ([]byte, error) {
+	e := &encodeState{}
+	err := e.marshal("", data)
+	return e.buffer.Bytes(), err
+}
+
+func (e *encodeState) marshal(prefix string, data interface{}) error {
+	if e.writeBegan {
+		e.writeBegan = false
+	}
+	if e.buffer == nil {
+		e.buffer = bytes.NewBuffer(nil)
+	}
+
+	value := reflect.ValueOf(data)
+	if value.Kind() == reflect.Ptr {
+		return e.marshalPtr(prefix, value.Interface())
+	}
+
+	if value.Kind() == reflect.Struct {
+		return e.marshalStruct(prefix, value.Interface())
+	}
+
+	if value.Kind() == reflect.Map {
+		return e.marshalMap(prefix, value.Interface())
+	}
+
+	return fmt.Errorf("don't know how to marshal this type")
+}
+
+func (e *encodeState) writeTab() error {
+	if !e.writeBegan {
+		return nil
+	}
+
+	_, err := e.buffer.WriteRune('\t')
+	return err
+}
+
+func (e *encodeState) marshalPtr(prefix string, data interface{}) error {
+	value := reflect.ValueOf(data)
+	value = value.Elem()
+
+	if value.Kind() == reflect.Struct {
+		return e.marshalStruct(prefix, value.Interface())
+	}
+
+	return fmt.Errorf("don't know how to marshal this pointer")
+}
+
+func (e *encodeState) marshalStruct(prefix string, data interface{}) error {
+	ref := reflect.TypeOf(data)
+	value := reflect.ValueOf(data)
+
+	for i := 0; i < value.NumField(); i++ {
+		fieldType := ref.Field(i)
+		// Ignore unexported fields
+		if fieldType.PkgPath != "" {
+			continue
+		}
+		// Get the key for this field
+		var key string
+		if key = fieldType.Tag.Get("kv"); key == "-" {
+			continue
+		} else if key == "" {
+			key = fieldType.Name
+		}
+		e.writeTab()
+
+		field := value.Field(i)
+		if field.Kind() == reflect.Ptr || field.Kind() == reflect.Struct || field.Kind() == reflect.Map {
+			if len(key) > 0 {
+				key = key + "."
+			}
+			err := e.marshal(key, field.Interface())
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		value := stringify(field.Interface())
+
+		if _, err := e.buffer.WriteString(fmt.Sprintf("%s%s=%s", prefix, key, value)); err != nil {
+			return err
+		}
+		e.writeBegan = true
+	}
+
+	return nil
+}
+
+func (e *encodeState) marshalMap(prefix string, data interface{}) error {
+	value := reflect.ValueOf(data)
+	keys := value.MapKeys()
+	for _, v := range keys {
+		e.writeTab()
+		mapKey, _ := strconv.Unquote(stringify(v.Interface()))
+		mapValue := stringify(value.MapIndex(v).Interface())
+		if _, err := e.buffer.WriteString(fmt.Sprintf("%s%s=%s", prefix, mapKey, mapValue)); err != nil {
+			return err
+		}
+		e.writeBegan = true
+	}
+
+	return nil
+}
+
+func stringify(data interface{}) string {
+	value := ""
+	switch t := data.(type) {
+	case bool:
+		value = strconv.FormatBool(t)
+	case rune:
+		value = strconv.QuoteRune(t)
+	case time.Time:
+		value = t.UTC().Format(time.RFC3339)
+	case int:
+		value = strconv.Itoa(int(t))
+    case fmt.Stringer:
+		value = t.String()
+	case string:
+		value = strconv.Quote(t)
+	case []string:
+		value = strconv.Quote(strings.Join(t, ";"))
+	default:
+		value = "n/a"
+	}
+	return value
+}
---
+++ kv_test.go
@@ -0,0 +1,76 @@
+package gogol
+
+import (
+	"testing"
+	"time"
+)
+
+type foo struct {
+	I   int
+	S   string
+	R   rune
+	B   bool
+	T   time.Time
+	D   time.Duration
+	foo bool // unexported, so won't be logged
+	Foo bool `kv:"-"`      // "-" means that the field won't be logged
+	Bar bool `kv:"foobar"` // change the key
+}
+
+type bar struct {
+	Bar int
+	Foo *foo
+}
+
+var f = &foo{
+	I: 34,
+	S: "all aboard!",
+	R: 'b',
+	T: time.Date(2006, 8, 24, 02, 30, 0, 0, time.UTC),
+	D: 30 * time.Millisecond}
+
+var b = &bar{Bar: 0, Foo: f}
+
+func TestMarshalStruct(t *testing.T) {
+	m, err := Marshal(f)
+	if err != nil {
+		t.Error("marshal failed", err)
+	}
+	ex := "I=34\tS=\"all aboard!\"\tR='b'\tB=false\tD=30ms\tfoobar=false"
+	if string(m) != ex {
+		t.Error("Marhshal returned a wrong result")
+	}
+
+	m, err = Marshal(*f)
+	if err != nil {
+		t.Error("marshal failed", err)
+	}
+	if string(m) != ex {
+		t.Error("Marhshal returned a wrong result")
+	}
+}
+
+func TestMarshalDeepStruct(t *testing.T) {
+	m, err := Marshal(b)
+	if err != nil {
+		t.Error("marshal failed", err)
+	}
+	ex := "Bar=0\tFoo.I=34\tFoo.S=\"all aboard!\"\tFoo.R='b'\tFoo.B=false\tFoo.D=30ms\tFoo.foobar=false"
+	if string(m) != ex {
+		t.Error("Marhshal returned a wrong result")
+	}
+}
+
+func TestMarshalMap(t *testing.T) {
+	m, err := Marshal(map[string]interface{}{
+		"foo":  true,
+		"rune": 'c',
+		"bar":  time.Date(2006, 8, 24, 02, 30, 0, 0, time.UTC),
+	})
+	if err != nil {
+		t.Error("marshal failed.", err)
+	}
+	if string(m) != "foo=true\trune='c'\tbar=2006-08-24T02:30:00Z" {
+		t.Error("marshalisation is wrong")
+	}
+}
