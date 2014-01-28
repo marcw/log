c92806a3995304ffdbb32628bef1c5ff1fdb503f
Marc Weistroff <marc@weistroff.net>
2014-01-28 19:21:54 +0100

    golint

--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 /*
-log is a log package heavily inspired by monolog/logbook.
+Package log implements logging in a similar fashion than monolog/logbook
 
     Usage:
 
